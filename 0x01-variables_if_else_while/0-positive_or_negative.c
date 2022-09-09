6d173838788fbeb8e47286edf7b40e8e1e1635e1
Your Nameroot@896cf839cf9a:/alx-pre_course# git add .root@896cf839cf9a:/alx-pre_course# git commit -m 'My first commit'[master (root-commit) 98eef93] My first commit1 file changed, 1 insertion(+)create mode 100644 README.mdroot@896cf839cf9a:/alx-pre_course# git pushEnumerating objects: 3, done.Counting objects: 100% (3/3), done.Writing objects: 100% (3/3), 212 bytes | 212.00 KiB/s, done.Total 3 (delta 0), reused 0 (delta 0)To https://github.com/Saddamzy001/alx-pre_course.git * [new branch]      master - mastegit config --global user.name Your <saddamhusseini1234@gmail.com>
2022-09-09 14:16:00 -0700

    c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -3,7 +3,7 @@
 #include <stdio.h>
 
 /**
- main - assign a random number to the variable n each time it is executed
+ *main - assign a random number to the variable n each time it is executed
  *if the number is greater than 0: is positive, if the number is 0: is zero
  *if the number is less than 0: is negative, followed by a new line
  *Return: 0
@@ -12,7 +12,9 @@
 int main(void)
 {
 	int n;
+
 	srand(time(0));
+
 	n = rand() - RAND_MAX / 2;
 	if (n > 0)
 	{
