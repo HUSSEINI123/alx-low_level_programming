57ad2ce9fed20cb6b6147fd9da420e17568f60b8
Your Nameroot@896cf839cf9a:/alx-pre_course# git add .root@896cf839cf9a:/alx-pre_course# git commit -m 'My first commit'[master (root-commit) 98eef93] My first commit1 file changed, 1 insertion(+)create mode 100644 README.mdroot@896cf839cf9a:/alx-pre_course# git pushEnumerating objects: 3, done.Counting objects: 100% (3/3), done.Writing objects: 100% (3/3), 212 bytes | 212.00 KiB/s, done.Total 3 (delta 0), reused 0 (delta 0)To https://github.com/Saddamzy001/alx-pre_course.git * [new branch]      master - mastegit config --global user.name Your <saddamhusseini1234@gmail.com>
2022-09-15 09:36:24 -0700

    c

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -2,14 +2,11 @@
 
 /**
  * _isupper - function that checks for uppercase character.
- * @c: int type number
+ * @c: the character to be checked
  * Return: 1 if c is uppercase, 0 otherwise
  */
 
 int _isupper(int c)
 {
-	if (c >= 65 && c <= 91)
-		return (1);
-	else
-		return (0);
+	return (c >= 'A' && c <= 'Z');
 }
