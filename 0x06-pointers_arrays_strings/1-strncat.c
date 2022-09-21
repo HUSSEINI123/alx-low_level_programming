816bf49f83bfade8aa088c25d8521dbbe00574cd
Your Nameroot@896cf839cf9a:/alx-pre_course# git add .root@896cf839cf9a:/alx-pre_course# git commit -m 'My first commit'[master (root-commit) 98eef93] My first commit1 file changed, 1 insertion(+)create mode 100644 README.mdroot@896cf839cf9a:/alx-pre_course# git pushEnumerating objects: 3, done.Counting objects: 100% (3/3), done.Writing objects: 100% (3/3), 212 bytes | 212.00 KiB/s, done.Total 3 (delta 0), reused 0 (delta 0)To https://github.com/Saddamzy001/alx-pre_course.git * [new branch]      master - mastegit config --global user.name Your <saddamhusseini1234@gmail.com>
2022-09-21 12:14:38 -0700

    c

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,14 +1,27 @@
 #include "main.h"
 /**
- * swap_int - swaps the values pointed to by arguments
- * @a: pointer argument 1
- * @b: pointer argument 2
+ * _strncat - function to concatenate two strings and return a new string
+ * with n elements of the appended string
+ * @dest: string 1
+ * @src: string 2
+ * @n: number of chars to include from appended string
+ * Return: char pointer
  */
 
-void swap_int(int *a, int *b)
+char *_strncat(char *dest, char *src, int n)
 {
-	int con = *a;
+	int i = 0, j = 0;
+	char *p;
 
-	*a = *b;
-	*b = con;
+	while (*(dest + i) != '\0')
+		i++;
+	while (!(*(src + j) == '\0' || j == n))
+	{
+		*(dest + i) = *(src + j);
+		i++;
+		j++;
+	}
+	*(dest + i) = '\0';
+	p = dest;
+	return (p);
 }
