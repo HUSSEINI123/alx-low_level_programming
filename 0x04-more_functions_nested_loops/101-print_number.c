c2d64f335bcbfdaa71b6bd3cbbb08490653837af
Your Nameroot@896cf839cf9a:/alx-pre_course# git add .root@896cf839cf9a:/alx-pre_course# git commit -m 'My first commit'[master (root-commit) 98eef93] My first commit1 file changed, 1 insertion(+)create mode 100644 README.mdroot@896cf839cf9a:/alx-pre_course# git pushEnumerating objects: 3, done.Counting objects: 100% (3/3), done.Writing objects: 100% (3/3), 212 bytes | 212.00 KiB/s, done.Total 3 (delta 0), reused 0 (delta 0)To https://github.com/Saddamzy001/alx-pre_course.git * [new branch]      master - mastegit config --global user.name Your <saddamhusseini1234@gmail.com>
2022-09-28 15:55:24 -0700

    c

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,26 +1,58 @@
 #include "main.h"
-
 /**
- * print _number - prints number using _putchar
+ * print_number - prints a number using _putchar
  * @n: number to print
  * Return: void
  */
+
 void print_number(int n)
 {
-	unsigned int n1;
+	unsigned int un = 0;
 
 	if (n < 0)
 	{
-		n1 = -n;
-		_putchat('-');
+		if (n < 1000000000)
+		n = -n;
+		un = n;
+		_putchar(45);
+		num_to_char(n);
 	}
 	else
 	{
-		n1 = n;
+		un = n;
+		num_to_char(un);
 	}
-	if (n1 / 10)
+}
+
+/**
+ * num_to_char - transforms a number with 1 or more digits into a char
+ * @n: number to print
+ * Return: void
+ */
+
+void num_to_char(unsigned int n)
+{
+	unsigned int d = 10;
+	if (n < d)
 	{
-		print_number(n1 / 10);
+		_putchar('0' + n);
 	}
-	_putchar((n1 % 10) + '0');
+	else
+	{
+		while (n >= d)
+		{
+			d *= 10;
+			if (d == 1000000000)
+				break;
+		}
+		if (!(d == 1000000000) || n == 123456789)
+			d /= 10;
+		_putchar('0' + n / d);
+		while (d != 10)
+		{
+			d /= 10;
+			_putchar('0' + (n / d) % 10);
+		}
+		_putchar('0' + n % 10);
+	}
 }
