55beaaf0c8942629d04db27845f1bc6a334f7593
Your Nameroot@896cf839cf9a:/alx-pre_course# git add .root@896cf839cf9a:/alx-pre_course# git commit -m 'My first commit'[master (root-commit) 98eef93] My first commit1 file changed, 1 insertion(+)create mode 100644 README.mdroot@896cf839cf9a:/alx-pre_course# git pushEnumerating objects: 3, done.Counting objects: 100% (3/3), done.Writing objects: 100% (3/3), 212 bytes | 212.00 KiB/s, done.Total 3 (delta 0), reused 0 (delta 0)To https://github.com/Saddamzy001/alx-pre_course.git * [new branch]      master - mastegit config --global user.name Your <saddamhusseini1234@gmail.com>
2022-10-30 12:33:37 -0700

    c

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -15,10 +15,10 @@ int get_bit(unsigned long int n, unsigned int index)
 		return (0);
 	for (i = 0; i <= 63; n >>= 1, i++)
 	{
-	if (index == i)
-		{
-			return (n & 1);
-		}
+		if (index == i)
+			{
+				return (n & 1);
+			}
 	}
 	return (-1);
 }
