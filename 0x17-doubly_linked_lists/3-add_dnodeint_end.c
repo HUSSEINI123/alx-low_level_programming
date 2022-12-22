d1c7190ff401f93baa759d593e6bd4f9bb23e05f
Your Nameroot@896cf839cf9a:/alx-pre_course# git add .root@896cf839cf9a:/alx-pre_course# git commit -m 'My first commit'[master (root-commit) 98eef93] My first commit1 file changed, 1 insertion(+)create mode 100644 README.mdroot@896cf839cf9a:/alx-pre_course# git pushEnumerating objects: 3, done.Counting objects: 100% (3/3), done.Writing objects: 100% (3/3), 212 bytes | 212.00 KiB/s, done.Total 3 (delta 0), reused 0 (delta 0)To https://github.com/Saddamzy001/alx-pre_course.git * [new branch]      master - mastegit config --global user.name Your <saddamhusseini1234@gmail.com>
2022-12-22 07:30:09 -0800

    c

--- 0x17-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -14,7 +14,7 @@ dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 
 	new = malloc(sizeof(dlistint_t));
 	if (new == NULL)
-	return (NULL);
+		return (NULL);
 	new->n = n;
 	new->next = NULL;
 	if (*head == NULL)
@@ -26,7 +26,7 @@ dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 	last = *head;
 	while (last->next != NULL)
 		last = last->next;
-		last->next = new;
-		new->prev = last;
+	last->next = new;
+	new->prev = last;
 	return (new);
 }
